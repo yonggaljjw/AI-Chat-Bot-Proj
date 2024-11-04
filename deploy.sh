055af495535ca801f3aa75452bf0e12b348485c4
홍석범 <brent93.dev@gmail.com>
2024-11-04 17:17:29 +0900

    parse shell script and modify airflow pw and username

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,21 @@ cp requirements.txt project/WEB/requirements.txt \
 # Build the django project to the server and run the docker-compose (Deploy the django project)
 printf "Building the django project to the server\n"
 cd project/WEB \
-    && docker-compose up -d --build \
+    && docker compose up -d --build \
     && printf "Done! Deploy also ~\n"
 
-# Deploy the Airflow project and ELK stack
-printf "Deploying the Airflow project and ELK stack\n"
+# Move to the Airflow project and ELK stack directory
 cd ../../ \
     && cd DB
-    && docker-compose up -d \
-    && printf "Done! Deployed the Airflow project and ELK stack\n"
+
+# Deploy the Airflow project
+cd airflow \
+    && printf "Deploying the Airflow project\n" \
+    && docker compose up -d \
+    && printf "Done! \n"
+
+# # Deploy the ELK stack
+# cd ../elk \
+#     && printf "Deploying the ELK stack\n" \
+#     && docker compose up -d \
+#     && printf "Done! \n"
