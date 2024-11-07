449dfc313b7b8ed27d816a9f43c8ca5dd9f5683b
eunchaipark <168709547+eunchaipark@users.noreply.github.com>
2024-11-07 14:14:18 +0900

    for chart 1

--- project/WEB/woodjango/urls.py
+++ project/WEB/woodjango/urls.py
@@ -19,12 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 from chatbot import views
 from django_plotly_dash import DjangoDash
-
-dash_app = DjangoDash('wordcloud_app')
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.index, name='index'),
     path("chat/", views.chat, name="chat"),
-    path('dash/', views.dash_app, name='show_dash'),
-]
+    path("django_plotly_dash/", include("django_plotly_dash.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
