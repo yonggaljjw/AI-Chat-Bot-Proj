a909d72684ac8fb2ee826d32e970b5d73627157e
eunchaipark <168709547+eunchaipark@users.noreply.github.com>
2024-11-07 15:06:08 +0900

    로그인 창 생성 및 db는 일단 로컬 연결

--- project/WEB/woodjango/urls.py
+++ project/WEB/woodjango/urls.py
@@ -24,7 +24,8 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     path("chat/", views.chat, name="chat"),
     path("django_plotly_dash/", include("django_plotly_dash.urls")),
+    path('', include('authenticate.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
