5ae3eedff16eaf0b89f807135a16adf75a578080
awesome98 <ghtjq3824@gmail.com>
2024-11-13 16:28:02 +0900

    dags

--- project/FISA/hoseop/H_01_wooricard_data.py
+++ project/FISA/hoseop/H_01_wooricard_data.py
@@ -22,6 +22,82 @@ client = OpenSearch(
     verify_certs=False
 )
 
+   # 1. 'edu_data_F_cleaned' CSV 파일을 OpenSearch에 업로드하는 함수
+# def upload_wooricard_data():
+#     # ./dags/package/dddd.csv
+#     file_path = '/opt/airflow/data/wooricard_data/edu_data_F_cleaned.csv'  # 실제 경로로 변경하세요.
+#     df = pd.read_csv(file_path)
+
+#     # 인덱스 생성 및 매핑 설정
+#     if not client.indices.exists(index='wooricard_data'):
+#         client.indices.create(
+#             index='wooricard_data',
+#             body={
+#                 "mappings": {
+#                     "properties": {
+#                         "고객번호": {"type": "text"},
+#                         "연령대": {"type": "text"},
+#                         "성별": {"type": "keyword"},
+#                         "회원등급": {"type": "keyword"},
+#                         "거주지역_1": {"type": "text"},
+#                         "라이프스테이지": {"type": "keyword"},
+#                         "총이용금액": {"type": "int"},
+#                         "신용카드이용금액": {"type": "int"},
+#                         "체크카드이용금액": {"type": "int"},
+#                         "가전/가구/주방용품": {"type": "int"},
+#                         "보험/병원": {"type": "int"},
+#                         "사무통신/서적/학원": {"type": "int"},
+#                         "여행/레져/문화": {"type": "int"},
+#                         "요식업": {"type": "int"},
+#                         "용역/수리/건축자재": {"type": "int"},
+#                         "유통": {"type": "int"},
+#                         "보건위생": {"type": "int"},
+#                         "의류/신변잡화": {"type": "int"},
+#                         "자동차/연료/정비": {"type": "int"},
+#                         "가구": {"type": "int"},
+#                         "가전제품": {"type": "int"},
+#                         "건물및시설관리": {"type": "int"},
+#                         "건축/자재": {"type": "int"},
+#                         "광학제품": {"type": "int"},
+#                         "농업": {"type": "int"},
+#                         "레져업소": {"type": "int"},
+#                         "레져용품": {"type": "int"},
+#                         "문화/취미": {"type": "int"},
+#                         "보건/위생": {"type": "int"},
+#                         "보험": {"type": "int"},
+#                         "사무/통신기기": {"type": "int"},
+#                         "서적/문구": {"type": "int"},
+#                         "수리서비스": {"type": "int"},
+#                         "숙박업": {"type": "int"},
+#                         "신변잡화": {"type": "int"},
+#                         "여행업": {"type": "int"},
+#                         "연료판매": {"type": "int"},
+#                         "용역서비스": {"type": "int"},
+#                         "유통업비영리": {"type": "int"},
+#                         "유통업영리": {"type": "int"},
+#                         "음식료품": {"type": "int"},
+#                         "의료기관": {"type": "int"},
+#                         "의류": {"type": "int"},
+#                         "일반/휴게음식": {"type": "int"},
+#                         "자동차정비/유지": {"type": "int"},
+#                         "자동차판매": {"type": "int"},
+#                         "주방용품": {"type": "int"},
+#                         "직물": {"type": "int"},
+#                         "학원": {"type": "int"},
+#                         "회원제형태업소": {"type": "int"}
+#                     }
+#                 }
+#             }
+#         )
+    
+#     oml.pandas_to_opensearch(
+#         pd_df=df,
+#         os_client=client,
+#         os_dest_index="wooricard_data",
+#         os_if_exists="append",
+#         os_refresh=True
+#     )
+
 # 'age_payment' CSV 파일을 OpenSearch에 업로드하는 함수
 def upload_age_payment_data():
     file_path = '/opt/airflow/dags/data/age_payment.csv'  # 실제 경로로 변경하세요.
@@ -229,7 +305,6 @@ with DAG(
         task_id="upload_age_payment",
         python_callable=upload_age_payment_data,
     )
-
 
     upload_male_task = PythonOperator(
         task_id="upload_male",
