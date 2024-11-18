dc8d86c011b586a28b8d6fec192adc04d9ddc227
eunchaipark <168709547+eunchaipark@users.noreply.github.com>
2024-11-18 14:33:14 +0900

    Commit local changes before pulling upstream

--- project/WEB/authenticate/urls.py
+++ project/WEB/authenticate/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
     path('change_password/', views.change_password, name='change_password'),
-    path('index/', views.index, name='index')
+    path('dashboard_hoseop/', views.dashboard_hoseop, name='dashboard_hoseop'),
+    path("chatbot/send/", views.chat, name="chatbot_send"),
 ]
