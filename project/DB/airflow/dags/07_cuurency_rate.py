53a68d2c3167fa0e21efdd7b926a5e5d21b96de8
yonggaljjw <jjw6270@gmail.com>
2024-11-19 12:31:57 +0900

    dag수정 1차

--- project/DB/airflow/dags/07_cuurency_rate.py
+++ project/DB/airflow/dags/07_cuurency_rate.py
@@ -6,37 +6,32 @@ from dotenv import load_dotenv
 import os
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from opensearchpy import OpenSearch
-import opensearch_py_ml as oml
+import pymysql
+from sqlalchemy import create_engine
 
 
 load_dotenv()
 
+username = os.getenv('sql_username')
+password = os.getenv('sql_password')
+host = os.getenv('sql_host')
+port = os.getenv('sql_port')
 
-host = os.getenv("HOST")
-port = os.getenv("PORT")
-auth = (os.getenv("OPENSEARCH_ID"), os.getenv("OPENSEARCH_PASSWORD"))
+engine = create_engine(f"mysql+pymysql://{username}:{password}@{host}:{port}/team5")
 
 url = 'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON'
-authkey = os.getenv("cur_authkey")
+authkey = os.getenv('cur_authkey')
 data = 'AP01'
-
-client = OpenSearch(
-    hosts = [{'host': host, 'port': port}],
-    http_auth = auth,
-    use_ssl = True,
-    verify_certs = False
-)
-
-
+date = datetime.today().strftime('%Y-%m-%d')
 
 def extract_currency_rate() :
     params = {
         'authkey': authkey,
-        'searchdate' : datetime.today(),
+        'searchdate' : date,
         'data' : data}
-    response = pd.DataFrame(requests.get(url, params=params).json())
-    response['date'] = datetime.today()
+    
+    response = pd.DataFrame(requests.get(url, params=params, verify=False).json())
+    response['date'] = date
     response = response.drop(columns=['result'])
 
     response['ttb'] = response['ttb'].apply(lambda x : x.replace(",",""))
@@ -46,6 +41,7 @@ def extract_currency_rate() :
     response['kftc_bkpr'] = response['kftc_bkpr'].apply(lambda x : x.replace(",",""))
     response['kftc_deal_bas_r'] = response['kftc_deal_bas_r'].apply(lambda x : x.replace(",",""))
 
+    response['date'] = pd.to_datetime(response['date'], format='%Y-%m-%d')
     response['ttb'] = response['ttb'].astype('float')
     response['tts'] = response['tts'].astype('float')
     response['deal_bas_r'] = response['deal_bas_r'].astype('float')
@@ -57,29 +53,10 @@ def extract_currency_rate() :
 
     return response
 
-## 데이터 적재를 위한 bulk_action 함수 생성
-def create_bulk_actions(df, index_name):
-    actions = []
-    for index, row in df.iterrows():
-        # Index action
-        action = {
-            "_index": index_name,
-            "_source": row.to_dict()
-        }
-        actions.append(action)
-    return actions
+def upload_currency_rate() :
+    df = extract_currency_rate()
 
-
-# Bulk API를 위한 작업 생성
-def bulk_insert() :
-    actions = create_bulk_actions(extract_currency_rate(), 'currency_rate')
-    # Bulk API 호출
-    if actions:
-        # helpers.bulk(es, actions)
-        helpers.bulk(client, actions)
-        print(f"{len(actions)}개의 문서가 OpenSearch에 업로드되었습니다.")
-    else:
-        print("업로드할 문서가 없습니다.")
+    df.to_sql('currency_rate', con=engine, if_exists='append', index=False)
 
 
 # Airflow DAG 기본 설정
@@ -97,13 +74,13 @@ with DAG(
     schedule_interval='@daily',
     start_date=datetime(2010, 1, 1),
     catchup=False,
-    tags=['Opensearch', 'currency', 'data']
+    tags=['Mysql', 'currency', 'data']
 ) as dag:
     
     # PythonOperator 설정
     t1 = PythonOperator(
     task_id="currency_rate_upload",
-    python_callable=bulk_insert,
+    python_callable=upload_currency_rate,
     )
 
     t1
