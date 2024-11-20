4f11ecec666af687c0f7f627d8b124d43a3de297
JiyeonJeong02 <wlduswjd1@gmail.com>
2024-11-20 18:36:25 +0900

    add new data_card sales

--- project/WEB/chatbot/views_main.py
+++ project/WEB/chatbot/views_main.py
@@ -1,18 +1,19 @@
 from .views_fred import *
+from .views_card_sales import *
 from .views_hoseop import *
 # from .views_eunji import *
 # from .views_eunchae import *
 
 
 def dashboard_view(request):
-
+    # 우리카드 데이터 분석 - 호섭
     top10_level_html = top10_level_view()
     lifestage_distribution_html = lifestage_distribution_view()
     age_dist_html, lifestage_dist_html = age_and_life_stage_distribution_view()
     male_chart_html, female_chart_html = gender_expense_distribution_view()
     age_payment_html = age_payment_distribution_view()
     age_category_html = age_category_top5_view()
-
+    # 거시경제 지표 - 지연
     gdp_rates_html = gdp_and_rates_view()
     price_indicators_html = price_indicators_view()
     consumer_trends_html = consumer_trends_view()
@@ -21,6 +22,7 @@ def dashboard_view(request):
 
     # 템플릿에 전달
     return render(request, "main.html", {
+        # 우리카드 데이터 분석 - 호섭
         "top10_level_html" : top10_level_html,
         "lifestage_distribution_html" : lifestage_distribution_html,
         "age_dist_html" : age_dist_html,
@@ -29,6 +31,7 @@ def dashboard_view(request):
         "female_chart_html" : female_chart_html,
         "age_payment_html" : age_payment_html,
         "age_category_html" : age_category_html,
+        # 거시경제 지표 - 지연
         "gdp_rates_html": gdp_rates_html,
         "price_indicators_html": price_indicators_html,
         "consumer_trends_html": consumer_trends_html,
