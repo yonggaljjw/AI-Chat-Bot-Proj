816388071f963720a1b0046535c08ead6440407f
awesome98 <ghtjq3824@gmail.com>
2024-11-20 15:34:04 +0900

    changing django structure

--- project/WEB/chatbot/urls.py
+++ project/WEB/chatbot/urls.py
@@ -1,17 +1,10 @@
 from django.urls import path
-from . import views_hoseop
-from . import views_fred
+from . import views_main
 from . import views_chatbot
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('index/', views_hoseop.dashboard_view, name='index'),
-    path('fred/', views_fred.fred_dashboard_view, name='fred'),
+    path('index/', views_main.dashboard_view, name='index'),
     path('chatbot/send/', views_chatbot.chatbot_response, name='chatbot_send'),
-    # path('dashboard1/', views.dashboard1, name='dashboard1'),
-    # path('dashboard2/', views.dashboard2, name='dashboard2'),
-    # path('dashboard3/', views.dashboard3, name='dashboard3'),
-    # path('dashboard4/', views.dashboard4, name='dashboard4'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- project/WEB/chatbot/views_hoseop.py
+++ project/WEB/chatbot/views_hoseop.py
@@ -6,7 +6,6 @@ import pandas as pd
 from django.conf import settings  # settings.py의 경로 설정 사용
 from sqlalchemy import create_engine
 import pymysql
-from .views_fred import *
 
 
 def load_csv_data():
@@ -311,40 +310,3 @@ def age_category_top5_view():
 
     # 그래프를 HTML로 변환 후 리턴
     return to_html(fig, full_html=False)
-
-def dashboard_view(request):
-
-    fred_dashboard_view(request)
-
-    top10_level_html = top10_level_view()
-    lifestage_distribution_html = lifestage_distribution_view()
-    age_dist_html, lifestage_dist_html = age_and_life_stage_distribution_view()
-    male_chart_html, female_chart_html = gender_expense_distribution_view()
-    age_payment_html = age_payment_distribution_view()
-    age_category_html = age_category_top5_view()
-
-    # if fred_data.empty:
-    #     return render(request, "dashboard_hoseop.html", {"error_message": "데이터를 불러올 수 없습니다."})
-
-    gdp_rates_html = gdp_and_rates_view()
-    price_indicators_html = price_indicators_view()
-    consumer_trends_html = consumer_trends_view()
-    employment_trends_html = employment_trends_view()
-    economic_table_html = economic_indicators_table_view()
-
-    # 템플릿에 전달
-    return render(request, "dashboard_hoseop.html", {
-        "top10_level_html" : top10_level_html,
-        "lifestage_distribution_html" : lifestage_distribution_html,
-        "age_dist_html" : age_dist_html,
-        "lifestage_dist_html" : lifestage_dist_html,
-        "male_chart_html" : male_chart_html,
-        "female_chart_html" : female_chart_html,
-        "age_payment_html" : age_payment_html,
-        "age_category_html" : age_category_html,
-        "gdp_rates_html": gdp_rates_html,
-        "price_indicators_html": price_indicators_html,
-        "consumer_trends_html": consumer_trends_html,
-        "employment_trends_html": employment_trends_html,
-        "economic_table_html": economic_table_html
-        })
