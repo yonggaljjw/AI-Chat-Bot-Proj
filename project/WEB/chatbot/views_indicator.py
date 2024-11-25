517944f47e3b61ae80dae78a538fbd08b39e8e39
awesome98 <ghtjq3824@gmail.com>
2024-11-25 15:12:29 +0900

    cpi, pce index making

--- project/WEB/chatbot/views_indicator.py
+++ project/WEB/chatbot/views_indicator.py
@@ -188,6 +188,7 @@ def K__indicator() :
 
 
 def K_USD_indicator() :
+
     # 데이터 불러오기
     query = """
     SELECT 
@@ -195,6 +196,88 @@ def K_USD_indicator() :
     FROM currency_rate
     ORDER BY TIME desc
     LIMIT 2;
+    """
+    recent_two_rates = pd.read_sql(query, engine)
+
+    # 현재값과 직전값 할당
+    current_value = float(recent_two_rates.iloc[0].values[0])
+    previous_value = float(recent_two_rates.iloc[1].values[0])
+
+    # 변동값 색상 결정
+    change_value = float(recent_two_rates.iloc[0].values[0])
+    if change_value >= 0:
+        delta_color = 'red'
+    else:
+        delta_color = 'blue'
+
+    # 서브플롯 생성
+    fig = make_subplots(rows=1, cols=1, specs=[[{"type": "indicator"}]])
+
+
+    # 인디케이터 추가
+    fig.add_trace(go.Indicator(
+        mode="number+delta",
+        value=current_value,
+        delta={'reference': previous_value, 'relative': True, 'valueformat' : '.2f', 'font' : {'color': delta_color}},
+        # title={'text': variable_name, 'font': {'size': 20}, 'align': 'center'},
+        number={'font': {'size': 50}},
+        domain={'x': [0, 1], 'y': [0, 1]}
+    ))
+
+    # 레이아웃 업데이트
+    fig.update_layout(
+        # margin=dict(l=50, r=50, t=50, b=50),
+        height=400,
+        width=400
+    )
+    return to_html(fig)
+
+def K_cpi_indicator() :
+
+    # 데이터 불러오기
+    query = """
+    SELECT TOTAL FROM cpi_data ORDER BY TIME DESC LIMIT 2;
+    """
+    recent_two_rates = pd.read_sql(query, engine)
+
+    # 현재값과 직전값 할당
+    current_value = float(recent_two_rates.iloc[0].values[0])
+    previous_value = float(recent_two_rates.iloc[1].values[0])
+
+    # 변동값 색상 결정
+    change_value = float(recent_two_rates.iloc[0].values[0])
+    if change_value >= 0:
+        delta_color = 'red'
+    else:
+        delta_color = 'blue'
+
+    # 서브플롯 생성
+    fig = make_subplots(rows=1, cols=1, specs=[[{"type": "indicator"}]])
+
+
+    # 인디케이터 추가
+    fig.add_trace(go.Indicator(
+        mode="number+delta",
+        value=current_value,
+        delta={'reference': previous_value, 'relative': True, 'valueformat' : '.2f', 'font' : {'color': delta_color}},
+        # title={'text': variable_name, 'font': {'size': 20}, 'align': 'center'},
+        number={'font': {'size': 50}},
+        domain={'x': [0, 1], 'y': [0, 1]}
+    ))
+
+    # 레이아웃 업데이트
+    fig.update_layout(
+        # margin=dict(l=50, r=50, t=50, b=50),
+        height=400,
+        width=400
+    )
+    return to_html(fig)
+
+def K_pce_indicator() :
+
+    # 데이터 불러오기
+    query = """
+    SELECT DATA_VALUE FROM pce_data ORDER BY TIME DESC LIMIT 2;
     """
     recent_two_rates = pd.read_sql(query, engine)
 
