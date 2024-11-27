556e4eeca86594ce40b7c1dc9f31a291e70a7a10
홍석범 <brent93.dev@gmail.com>
2024-11-27 12:19:59 +0900

    layout dashboard fix done

--- project/WEB/chatbot/views_main.py
+++ project/WEB/chatbot/views_main.py
@@ -9,6 +9,7 @@ from .views_currency import *
 from .views_indicator import *
 from .views_fred import *
 from django.views.decorators.cache import cache_page
+import secrets
 
 
 def dashboard_view(request):
@@ -63,66 +64,42 @@ def dashboard_view(request):
 @cache_page(60 * 60)  
 def dashboard_view_practice(request):
     """tmp와 연동 + /tmp에서 확인"""
-    # 카드 소비 카테고리 - 호섭
-    gender_json = gender_view()
-    # 카드사 매출 정보 - 지연
-    card_total_sales_ladar_json =  card_total_sales_ladar_view()
-    create_card_heatmap_json = create_card_heatmap_view()
-    wooricard_sales_treemap_json = wooricard_sales_treemap_view()
-    # 법 - 은지
-    korean_law_html = korean_law_view()
-    # 여행
-    tour_servey_json = tour_servey()
-    visualize_travel_advice_html = visualize_travel_advice()
-    travel_trend_line_json = travel_trend_line()
-    # 환율
-    currency_rates_json = create_currency_view()
-    # indicator
-    bankrate_indicator_json = bankrate_indicator()
-    K_GDP_indicator_json = K_GDP_indicator()
-    K_cpi_indicator_json = K_cpi_indicator()
-    K_pce_indicator_json = K_pce_indicator()
-    K_USD_indicator_json = K_USD_indicator()
-    K_growth_indicator_json = K_growth_indicator()
-    #
-    cpi_card_predict_json = cpi_card_predict_view()
-    # 거시경제 지표 - 지연
-    economic_indicators_table_json = economic_indicators_table_view()
-    gdp_rates_json = gdp_and_rates_view()
-    price_indicators_json = price_indicators_view()
-    consumer_trends_json = consumer_trends_view()
-    employment_trends_json = employment_trends_view()
-
-    # 템플릿에 전달
-    return render(request, "tmp.html", {
-        "gender_json": gender_json,
+    nonce = secrets.token_hex(16)
+    context = {
+        'nonce': nonce,
+        # 카드 소비 카테고리 - 호섭
+        "gender_json": gender_view(),
         # 카드사 매출 정보 - 지연
-        "card_total_sales_ladar_json" : card_total_sales_ladar_json,
-        "create_card_heatmap_json" : create_card_heatmap_json,
-        "wooricard_sales_treemap_json" : wooricard_sales_treemap_json,
+        "card_total_sales_ladar_json" :  card_total_sales_ladar_view(),
+        "create_card_heatmap_json" : create_card_heatmap_view(),
+        "wooricard_sales_treemap_json" : wooricard_sales_treemap_view(),
         # 법 - 은지
-        "korean_law_html": korean_law_html,
-        "tour_servey_json":tour_servey_json,
-        "visualize_travel_advice_html" : visualize_travel_advice_html,
-        "travel_trend_line_json" : travel_trend_line_json,
+        "korean_law_html": korean_law_view(),
+        # 여행
+        "tour_servey_json": tour_servey(),
+        "visualize_travel_advice_html" : visualize_travel_advice(),
+        "travel_trend_line_json" : travel_trend_line(),
         # 환율
-        "currency_rates_json" : currency_rates_json,
+        "currency_rates_json" : create_currency_view(),
         # indicator
-        "bankrate_indicator_json" : bankrate_indicator_json,
-        "K_GDP_indicator_json" : K_GDP_indicator_json,
-        "K_cpi_indicator_json" : K_cpi_indicator_json,
-        "K_pce_indicator_json" : K_pce_indicator_json,
-        "K_USD_indicator_json" : K_USD_indicator_json,
-        "K_growth_indicator_json" : K_growth_indicator_json,
+        "bankrate_indicator_json" : bankrate_indicator(),
+        "K_GDP_indicator_json" : K_GDP_indicator(),
+        "K_cpi_indicator_json" : K_cpi_indicator(),
+        "K_pce_indicator_json" : K_pce_indicator(),
+        "K_USD_indicator_json" : K_USD_indicator(),
+        "K_growth_indicator_json" : K_growth_indicator(),
         #
-        "cpi_card_predict_json" : cpi_card_predict_json,
+        "cpi_card_predict_json" : cpi_card_predict_view(),
         # 거시경제 지표 - 지연
-        "gdp_rates_json": gdp_rates_json,
-        "price_indicators_json": price_indicators_json,
-        "consumer_trends_json": consumer_trends_json,
-        "employment_trends_json": employment_trends_json,
-        "economic_indicators_table_json" : economic_indicators_table_json,
-    })
+        "gdp_rates_json": gdp_and_rates_view(),
+        "price_indicators_json": price_indicators_view(),
+        "consumer_trends_json": consumer_trends_view(),
+        "employment_trends_json": employment_trends_view(),
+        "economic_indicators_table_json" : economic_indicators_table_view(),
+    }
+    response = render(request, "tmp.html", context)
+    response['Content-Security-Policy'] = f"script-src 'self' 'unsafe-eval' 'nonce-{nonce}' https://cdn.tailwindcss.com https://cdn.plot.ly;"
+    return response
 
 def dashboard_view_practice2(request):
     """tmp_origin과 연동 + /tmp_origin에서 확인"""
